db35b308e4af4cb84df4d7f9be876f3da32b9338
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-08-22 14:23:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 6.2
+let num2 = 10
+let multiply =  num1 * num2
+let random = 11
+Math.floor(Math.random()* 10)
+let num3 = 84
+let num4 = 20
+let mod = num3%num4
+let max = 20
+let till = 5
+Math.max [max, till]
